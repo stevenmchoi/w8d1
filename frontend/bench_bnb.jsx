3a1b83c565ceff1e552c9018d910d8fe8fe30e4b
Steven Choi <steven.m.choi@gmail.com>
2018-03-30 17:04:35 -0700

    Create session api util for signup, login, logout

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login, logout } from './util/session_api_util';
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+	const root = document.getElementById('root');
+	ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
