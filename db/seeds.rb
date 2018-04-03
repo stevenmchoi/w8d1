46e7eb7d8ee4cd09ae22ca5f20ac295650594632
Steven Choi <steven.m.choi@gmail.com>
2018-04-02 17:26:43 -0700

    Refactor Greeting to my original code after figuring out bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+## Test
+# getState()
+#
 # dispatch(login({
 # 		username: 'user1',
 # 		email: 'email1',
--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, signup } from './actions/session_actions';
-// import { login, logout, signup } from './util/session_api_util';
 import configureStore from './store/store';
 import Root from './components/root';
 
